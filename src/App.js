e794dcffdb23451e2fa4ac5c69281d30966b86b8
Bryce Pedroza <bapedroz@asu.edu>
2020-04-24 08:44:58 -0700

    moved react app up

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import Champions from './components/champions.js'
+import Header from './components/header.js'
+import Footer from './components/footer.js'
+
+import './App.css'
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
+
+
+function App() {
+
+  const myRef = useRef(null)
+  const executeScroll = () => {
+    console.log("scrolling")
+    scrollToRef(myRef)
+  }
+
+  return (
+    <div className="App">
+      <Header executeScroll={executeScroll}/>
+      <Champions/>
+      <Footer location={myRef}/>
+    </div>
+
+  );
+}
+
+export default App;
